0c29af9e6f642d207fec309a28ccdf1f8c34b694
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2023-03-18 21:53:56 +0000

    Deploying to gh-pages from @ OlenkaPasko/goit-js-hw-09@edc2d2f45f6064b7ef619c083a7061a4b9d6a432 🚀

--- 01-color-switcher.16a9771c.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(()=>{n=setInterval((()=>{}),1e3)})),e.addEventListener("click",(()=>{clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.16a9771c.js.map
---
+++ 01-color-switcher.865a42bd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){bodyColor.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.865a42bd.js.map
