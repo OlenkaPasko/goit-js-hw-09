d5b6dd9ed96fcc3c372869172a092042ddacef1b
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-19 00:08:26 +0200

    getRandomHexColor

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,15 @@
-
-// який після натискання кнопки «Start», раз на секунду
-//змінює колір фону < body > на випадкове значення
-//використовуючи інлайн стиль. Натисканням на кнопку
-//«Stop» зміна кольору фону повинна зупинятися.
-
-//refs.startBtn.addEventListener(`clic`)
-
-//})
-//refs.stopBtn.addEventListener(`clic`, () => {
-//timer.stop();
-//})
-//Враховуй, що на кнопку «Start» можна натиснути 
-//нескінченну кількість разів.Зроби так, щоб доки зміна теми запущена, 
-//кнопка «Start» була неактивною(disabled).
-
-//function btnChangeColor() {
-//}
-//function btnStopChangeColor() {}
- //nIntervId = null;
-
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
 
 let timerId = null;
 stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 startBtn.addEventListener('click', () => {
   startBtn.disabled = true;
@@ -40,10 +26,3 @@ stopBtn.addEventListener('click', () => {
 
   clearInterval(timerId);
 });
-
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
