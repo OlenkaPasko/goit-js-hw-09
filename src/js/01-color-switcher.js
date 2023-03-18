61c49ab98d1ffc0d7e20513fb2b08a4b3962c355
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-18 23:45:59 +0200

    btnStartChangeColor

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,3 @@
-const refs = {
-  startBtn: document.querySelector('button[data-start]'),
-  stopBtn: document.querySelector('button[data-stop]'),
-  body: document.querySelector('body'),
-};
-refs.startBtn.addEventListener(`clic`);
-refs.stopBtn.addEventListener(`clic`);
 
 // який після натискання кнопки «Start», раз на секунду
 //змінює колір фону < body > на випадкове значення
@@ -17,45 +10,35 @@ refs.stopBtn.addEventListener(`clic`);
 //refs.stopBtn.addEventListener(`clic`, () => {
 //timer.stop();
 //})
-//приклад з mdn
-let nIntervId;
+//Враховуй, що на кнопку «Start» можна натиснути 
+//нескінченну кількість разів.Зроби так, щоб доки зміна теми запущена, 
+//кнопка «Start» була неактивною(disabled).
 
-function btnChangeColor() {
-  // check if an interval has already been set up
-  if (!nIntervId) {
-    nIntervId = setInterval(flashText, 1000);
-  }
-}
+//function btnChangeColor() {
+//}
+//function btnStopChangeColor() {}
+ //nIntervId = null;
 
-function btnStartChangeColor() {
-  const oElem = document.getElementById('my_box');
-  oElem.className = oElem.className === 'go' ? 'stop' : 'go';
-}
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
-function btnStopTextColor() {
-  clearInterval(nIntervId);
-  // release our intervalID from the variable
-  nIntervId = null;
-}
+startBtn.addEventListener("click", () => {
+  
+  
+  timerId = setInterval(() => {
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  
+  clearInterval(timerId);
+});
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-/*const startBtn = document.querySelector(".js-start");
-const stopBtn = document.querySelector(".js-stop");
-let timerId = null;
-
-startBtn.addEventListener("click", () => {
-  timerId = setInterval(() => {
-    console.log(`I love async JS!  ${Math.random()}`);
-  }, 1000);
-});
-
-
-stopBtn.addEventListener("click", () => {
-  clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
-}); */
