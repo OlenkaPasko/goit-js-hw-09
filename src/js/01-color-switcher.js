80490b33270c42fa3c23625d5f7cb8c9bb4903bf
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-18 13:45:01 +0200

    const refs

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
 
+
+const refs = {
+startBtn: document.querySelector('button[data-start]'),
+stopBtn: document.querySelector('button[data-stop]')
+} 
+// який після натискання кнопки «Start», раз на секунду 
+//змінює колір фону < body > на випадкове значення
+//використовуючи інлайн стиль. Натисканням на кнопку 
+//«Stop» зміна кольору фону повинна зупинятися.
+
+refs.startBtn.addEventListener(`clic`, () => {
+    timer.start();
+})
+refs.stopBtn.addEventListener(`clic`, () => {
+    timer.stop();
+})
+
+
+    function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+/*const startBtn = document.querySelector(".js-start");
+const stopBtn = document.querySelector(".js-stop");
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+}); */
