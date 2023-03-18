84bc25c36c442fa4285044a7c36f1e50f4c7085a
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-18 23:26:00 +0200

    startBtn

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,48 @@
-
-
 const refs = {
-startBtn: document.querySelector('button[data-start]'),
-stopBtn: document.querySelector('button[data-stop]')
-} 
-// який після натискання кнопки «Start», раз на секунду 
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+refs.startBtn.addEventListener(`clic`);
+refs.stopBtn.addEventListener(`clic`);
+
+// який після натискання кнопки «Start», раз на секунду
 //змінює колір фону < body > на випадкове значення
-//використовуючи інлайн стиль. Натисканням на кнопку 
+//використовуючи інлайн стиль. Натисканням на кнопку
 //«Stop» зміна кольору фону повинна зупинятися.
 
-refs.startBtn.addEventListener(`clic`, () => {
-    timer.start();
-})
-refs.stopBtn.addEventListener(`clic`, () => {
-    timer.stop();
-})
+//refs.startBtn.addEventListener(`clic`)
 
+//})
+//refs.stopBtn.addEventListener(`clic`, () => {
+//timer.stop();
+//})
+//приклад з mdn
+let nIntervId;
 
-    function getRandomHexColor() {
+function btnChangeColor() {
+  // check if an interval has already been set up
+  if (!nIntervId) {
+    nIntervId = setInterval(flashText, 1000);
+  }
+}
+
+function btnStartChangeColor() {
+  const oElem = document.getElementById('my_box');
+  oElem.className = oElem.className === 'go' ? 'stop' : 'go';
+}
+
+function btnStopTextColor() {
+  clearInterval(nIntervId);
+  // release our intervalID from the variable
+  nIntervId = null;
+}
+
+function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
 
 /*const startBtn = document.querySelector(".js-start");
 const stopBtn = document.querySelector(".js-stop");
