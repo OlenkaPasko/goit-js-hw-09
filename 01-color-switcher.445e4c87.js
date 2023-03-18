e7f00c57ff264a7c05de5cf907406a80aeab805a
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2023-03-18 21:46:21 +0000

    Deploying to gh-pages from @ OlenkaPasko/goit-js-hw-09@61c49ab98d1ffc0d7e20513fb2b08a4b3962c355 🚀

--- 01-color-switcher.445e4c87.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("clic"),t.stopBtn.addEventListener("clic")}();
-//# sourceMappingURL=01-color-switcher.445e4c87.js.map
---
+++ 01-color-switcher.4b3df8fb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){}),1e3)})),e.addEventListener("click",(function(){clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.4b3df8fb.js.map
