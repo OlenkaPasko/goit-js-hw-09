1556757c53d3cdd41f74ad95aa74b0c78e641e15
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-24 15:01:54 +0200

    onClose

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,17 +11,48 @@ const refs = {
   dataSeconds: document.querySelector('data-seconds'),
 };
 
+refs.startBtn.disabled = true;
 
+let selectedDate = null;
 
-/*const options = {
+/*Метод onClose() з об'єкта параметрів викликається 
+щоразу під час закриття елемента інтерфейсу, 
+який створює flatpickr. Саме у ньому варто обробляти дату, 
+обрану користувачем. Параметр selectedDates - 
+це масив обраних дат, тому ми беремо перший елемент.
+
+Якщо користувач вибрав дату в минулому, покажи window.alert()
+ з текстом "Please choose a date in the future".
+Якщо користувач вибрав валідну дату (в майбутньому), 
+кнопка «Start» стає активною.
+Кнопка «Start» повинна бути неактивною доти, 
+доки користувач не вибрав дату в майбутньому.
+Натисканням на кнопку «Start» починається відлік часу 
+до обраної дати з моменту натискання.*/
+//обробка дати:
+//масив обраних дат
+//Метод getTime() повертає числове значення цієї дати 
+// Запис window.alert() зайвий, пишемо просто alert() або console.log()
+
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  
   onClose(selectedDates) {
+    if (selectedDates[0].getTime < Date.now()) {
+     Report.failure('Please choose a date in the future'); 
+    }
+    else {
+    selectedDate = selectedDates[0].getTime();
+      refs.startBtn.disabled = false;
+    }
     console.log(selectedDates[0]);
   },
-};*/
+    }
+  
+
 /*function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
