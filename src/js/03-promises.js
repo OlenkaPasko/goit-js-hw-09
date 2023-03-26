7e39f27295144e211c594ebcf247551bed5ae11a
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-26 22:03:16 +0300

    createPromise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,24 +11,27 @@ refs.form.addEventListener('click',);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      // Fulfill
-    } else {
-      // Reject
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+     }, delay);
   });
-  }
-/*Напиши скрипт, який на момент сабміту 
-форми викликає функцію createPromise(position, delay) 
-стільки разів, скільки ввели в поле amount. 
-Під час кожного виклику передай їй номер промісу (position), 
-що створюється, і затримку, 
-враховуючи першу затримку (delay), 
-введену користувачем, і крок (step).
+ }
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 
-const makePromise = (text, delay) => {
+/*const makePromise = (text, delay) => {
   return new Promise(resolve => {
     setTimeout(() => resolve(text), delay);
   });
@@ -41,4 +44,11 @@ Promise.all([promiseA, promiseB])
   .then(value => console.log(value)) //["promiseA value", "promiseB value"]
   .catch(error => console.log(error));
 
-*/
+Доповни код функції createPromise таким чином, 
+щоб вона повертала один проміс, який виконується 
+або відхиляється через delay часу. 
+Значенням промісу повинен бути об'єкт, 
+в якому будуть властивості position 
+і delay зі значеннями однойменних параметрів. 
+Використовуй початковий код функції для вибору того,
+ що потрібно зробити з промісом - виконати або відхилити.*/
