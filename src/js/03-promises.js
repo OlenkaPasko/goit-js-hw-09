42bc926d6f44b8953361ca636c0c91fcfb3810d2
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-26 12:59:08 +0300

    3звд

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  body: document.querySelector('body'),
+  form: document.querySelector('form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +17,10 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+/*Напиши скрипт, який на момент сабміту 
+форми викликає функцію createPromise(position, delay) 
+стільки разів, скільки ввели в поле amount. 
+Під час кожного виклику передай їй номер промісу (position), 
+що створюється, і затримку, 
+враховуючи першу затримку (delay), 
+введену користувачем, і крок (step).*/
