b727da274540fbb735ff7382ec517ea7b1ab60bf
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-26 13:05:50 +0300

    const shouldResolve

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,20 +7,38 @@ const refs = {
   step: document.querySelector('[name="step"]'),
   amount: document.querySelector('[name="amount"]'),
 };
-
+refs.form.addEventListener('click',);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+    } else {
+      // Reject
+    }
+  });
   }
-}
 /*Напиши скрипт, який на момент сабміту 
 форми викликає функцію createPromise(position, delay) 
 стільки разів, скільки ввели в поле amount. 
 Під час кожного виклику передай їй номер промісу (position), 
 що створюється, і затримку, 
 враховуючи першу затримку (delay), 
-введену користувачем, і крок (step).*/
+введену користувачем, і крок (step).
+
+const makePromise = (text, delay) => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(text), delay);
+  });
+};
+
+const promiseA = makePromise("promiseA value", 1000);
+const promiseB = makePromise("promiseB value", 3000);
+
+Promise.all([promiseA, promiseB])
+  .then(value => console.log(value)) //["promiseA value", "promiseB value"]
+  .catch(error => console.log(error));
+
+*/
